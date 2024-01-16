acb352875fddeafafa65b0743e20c411893d3efa
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-01-16 14:06:21 -0600

    HTTP homework

---
+++ HTTP/MathApp/Operation.cs
@@ -0,0 +1,25 @@
+﻿namespace MathApp
+{
+    public class Operation
+    {
+        public static int? Parse(int num1, int num2, string op)
+        {
+            switch (op)
+            {
+                case "add":
+                    return num1 + num2;
+                case "subtract":
+                    return num1 - num2;                
+                case "multiply":
+                    return num1 * num2;
+                case "divide":
+                    return num1 / num2;
+                case "modulus":
+                    return num1 % num2;
+                default:
+                    throw new ArgumentException($"invalid input for '{op}'");
+
+            }
+        }
+    }
+}
--- HTTP/MathApp/Program.cs
+++ HTTP/MathApp/Program.cs
@@ -1,4 +1,49 @@
+using MathApp;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.Run(async (HttpContext context) =>
+{
+    StreamReader reader = new StreamReader(context.Request.Body);
+    string body = await reader.ReadToEndAsync();
+
+    Dictionary<string, StringValues> queryDictionary = QueryHelpers.ParseQuery(body);
+    
+    if(
+    queryDictionary.TryGetValue("firstNumber", out StringValues firstNumber) &&
+    queryDictionary.TryGetValue("secondNumber", out StringValues secondNumber) &&
+    queryDictionary.TryGetValue("operation", out StringValues operation)
+    )
+    {
+        try
+        {
+            int num1 = Int32.Parse(firstNumber[0]);
+            int num2 = Int32.Parse(secondNumber[0]);
+            string op = operation[0];
+
+            context.Response.Headers["Content-Type"] = "text/html";
+            await context.Response.WriteAsync(Operation.Parse(num1, num2, op).ToString());
+        }
+        catch (ArgumentException)
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync($"Invalid input for 'operation");
+        }
+        catch (FormatException)
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync($"Invalid input for 'numbers'");
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"Invalid input for 'operation");
+        await context.Response.WriteAsync($"Invalid input for 'numbers'");
+    }
+});
 
 app.Run();
