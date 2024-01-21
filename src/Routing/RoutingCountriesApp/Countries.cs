c280b10cf06c9d932e12bb25c2fabdc855c69ca8
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-01-20 18:58:55 -0600

    finished routing assignment

---
+++ src/Routing/RoutingCountriesApp/Countries.cs
@@ -0,0 +1,28 @@
+﻿namespace RoutingCountriesApp
+{
+    public class Countries
+    {
+        private static readonly Dictionary<int, string> _countries = new Dictionary<int, string>()
+        {
+            {1, "United States"},
+            {2, "Canada"},
+            {3, "United Kingdom"},
+            {4, "India"},
+            {5, "Japan"},
+        };
+
+        public static string? GetCountryById(int id)
+        {
+            if(_countries.TryGetValue(id, out string country))
+            {
+                return country;
+            }
+            return null;
+        }
+
+        public static Dictionary<int, string> GetAll()
+        {
+            return _countries; 
+        }
+    }
+}
--- src/Routing/RoutingCountriesApp/Program.cs
+++ src/Routing/RoutingCountriesApp/Program.cs
@@ -1,8 +1,44 @@
+using System.Diagnostics.CodeAnalysis;
+using RoutingCountriesApp;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseRouter();
-app.UseEndpoints();
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("countries", async context =>
+    {
+        Dictionary<int, string> countries = Countries.GetAll();
+        foreach (KeyValuePair<int, string> country in countries)
+        {
+            await context.Response.WriteAsync($"{country.Key} - {country.Value}\n");
+        }
+    });
+
+    endpoints.MapGet("countries/{countryID:int:range(1,100)}", async context =>
+    {
+        int countryId = Convert.ToInt32(context.Request.RouteValues["countryID"]);
+        if (countryId > 100)
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("The CountryID should be between 1 and 100");
+        }
+
+        string? countryName = Countries.GetCountryById(countryId);
+
+        if (countryName != null)
+        {
+            await context.Response.WriteAsync($"{countryName}");
+        }
+        else
+        {
+            context.Response.StatusCode = 404;
+            await context.Response.WriteAsync("[No country]");
+        }
+
+
+    });
+});
 
 app.Run();
