3890575892a569fbbb42338a1c4a29e8f19819fd
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-01-26 23:35:16 -0600

    implemented model class

--- src/Controllers/IActionResultExample/Controllers/HomeController.cs
+++ src/Controllers/IActionResultExample/Controllers/HomeController.cs
@@ -1,43 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IActionResultExample.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IActionResultExample.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("bookstore")]
-        public IActionResult Index()
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        public IActionResult Index([FromQuery]int? bookid,[FromRoute] bool? isloggedin, [FromQuery] Book book)
         {
             // Book id should be applied 
-            if (!Request.Query.ContainsKey("bookid"))
+            if (bookid.HasValue == false)
             {
                 /*return Content("Book id is not supplied");*/
                 return BadRequest("Book id is not supplied");
             }
             // Book id can't be empty
-            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            if (bookid <= 0)
             {
                 return BadRequest("Book id can't be null or empty");
             }
 
-            // book id should be between 1 to 1000
-            int bookId = Convert.ToInt16(Request.Query["bookid"]);
-            if (bookId <= 0)
-            {
-                return BadRequest("Book id can't be less than or equal to zero");
-            }
-
-            if (bookId > 1000)
+            if (bookid > 1000)
             {
                 return NotFound("Book id can't be greater than 1000");
             }
 
             // isloggedin should be true
-            if (Convert.ToBoolean(Request.Query["isloggedin"]) == false)
+            if (isloggedin == false)
             {
                 /*return Unauthorized("User must be authenticated");*/
                 return StatusCode(401);
             }
 
+            return Content($"Book id: {book}, Book: {book}", "text/plain");
             /*return new RedirectToActionResult("Books", "Store", new { });*/ // 302 - found
             // return RedirectToAction("Books", "Store", new { Id = bookId });
             // return new RedirectToActionResult("Books", "Store", new { }, permanent: true); // 301 - Moved permanently
@@ -47,7 +42,8 @@ namespace IActionResultExample.Controllers
             return LocalRedirect($"store/books/{bookId}"); // 302 - Found
 
             return Redirect($"store/books/{bookId}"); // 302 - Found*/
-            return RedirectPermanent($"store/books/{bookId}");
+            /*return RedirectPermanent($"store/books/{bookid}");*/
+
         }
     }
 }
