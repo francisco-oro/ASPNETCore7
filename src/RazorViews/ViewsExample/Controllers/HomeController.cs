234dc403f7ec79b44927a269e184730cc0e5cad5
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-02-09 23:10:13 -0600

    added foreach loop in razor views

--- src/RazorViews/ViewsExample/Controllers/HomeController.cs
+++ src/RazorViews/ViewsExample/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace ViewsExample.Controllers
     public class HomeController : Controller
     {
         [Route("home")]
+        [Route("/")]
         public IActionResult Index()
         {
             return View(); // Views/Home/Index.cshtml
