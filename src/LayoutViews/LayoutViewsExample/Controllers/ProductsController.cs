21bfcc1ba61f163e0aba5e8809863c4d95034abf
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-02-15 00:55:48 -0600

    added nested layout views

---
+++ src/LayoutViews/LayoutViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Url: /search-products/1
+        [Route("search-products/{ProductID?}")]
+        public IActionResult Search(int? ProductID)
+        {
+            ViewBag.ProductID = ProductID;
+            return View();
+        }
+
+        [Route("order-product")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
