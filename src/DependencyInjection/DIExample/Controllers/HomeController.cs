eab955062534317b29574f960605d206328ec965
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-02-22 23:52:06 -0600

    Added services class library

---
+++ src/DependencyInjection/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
