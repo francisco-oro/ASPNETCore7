4664381528dfa06f25fd0ee852c84061d21bf2cb
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-02-28 23:02:49 -0600

    added environment injection in controller

--- src/Environments/EnvironmentsExample/Controllers/HomeController.cs
+++ src/Environments/EnvironmentsExample/Controllers/HomeController.cs
@@ -4,10 +4,20 @@ namespace EnvironmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        // constructor 
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
+            ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
             return View();
         }
+
     }
 }
--- src/Environments/EnvironmentsExample/Program.cs
+++ src/Environments/EnvironmentsExample/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseStaticFiles();
 app.UseRouting();
