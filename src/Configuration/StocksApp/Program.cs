e39289efe6ef83e6ec416b1fa9ce9d9946f657ec
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-03-04 10:14:29 -0600

    added httpclient to Finnhub

--- src/Configuration/StocksApp/Program.cs
+++ src/Configuration/StocksApp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 app.Run();
---
+++ src/Configuration/StocksApp/Services/MyService.cs
@@ -0,0 +1,25 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("cniv5dhr01qkq94g5ibgcniv5dhr01qkq94g5ic0"),
+                    Method = HttpMethod.Get
+                };
+
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            }
+        }
+    }
+}
