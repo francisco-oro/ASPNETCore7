dcb1f7d08900bbe7e5318b5b2e8ab0563433ec67
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-03-10 00:28:11 -0600

    added razor layout to CRUD Example project

---
+++ src/CRUD_Operations/CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,174 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+        //constructor
+        public CountriesServiceTest()
+        {
+            _countriesService = new CountriesService();
+        }
+
+        #region AddCountry
+        // When CountryAddRequest is null, it should throw ArgumentNullException
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            //Arrange 
+            CountryAddRequest? request = null;
+
+            //Assert 
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act 
+                _countriesService.AddCountry(request);
+            });
+        }
+        
+        [Fact]
+        // When the CountryName is null, it should throw ArgumentException
+        public void AddCountry_CountryNameIsNull()
+        {
+            //Arrange 
+            CountryAddRequest? request = new CountryAddRequest()
+            {
+                CountryName = null
+            };
+
+            //Assert 
+            Assert.Throws<ArgumentException>(() =>
+            {
+                // Act 
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        [Fact]
+        // When the CountryName is duplicate, it should throw ArgumentException
+        public void AddCountry_DuplicateCountryName()
+        {
+            //Arrange 
+            CountryAddRequest? request1 = new CountryAddRequest()
+            {
+                CountryName = "USA"
+            };
+            CountryAddRequest? request2 = new CountryAddRequest()
+            {
+                CountryName = "USA"
+            };
+            //Assert 
+            Assert.Throws<ArgumentException>(() =>
+            {
+                // Act
+                _countriesService.AddCountry(request1);
+                _countriesService.AddCountry(request2);
+            });
+        }
+
+        [Fact]
+        // When you supply proper country name, it should insert (add) the country to the existing list of countries
+        public void AddCountry_ProperCountryDetails()
+        {
+            //Arrange 
+            CountryAddRequest? request = new CountryAddRequest()
+            {
+                CountryName = "Japan"
+            };
+
+            //Act
+            CountryResponse response = _countriesService.AddCountry(request);
+            List<CountryResponse> countries_from_GetAllCountries = _countriesService.GetAllCountries();
+
+            //Assert
+            Assert.True(response.CountryID != Guid.Empty);
+            Assert.Contains(response, countries_from_GetAllCountries);
+        }
+
+        #endregion
+
+        #region GetAllCountries
+
+        [Fact]
+        //The list of countries should be empty by default (before adding any countries)
+        public void GetAllCountries_EmptyList()
+        {
+            //Acts 
+            List<CountryResponse> actual_country_response_list = _countriesService.GetAllCountries();
+
+            //Assert 
+            Assert.Empty(actual_country_response_list);
+        }
+
+        [Fact]
+        public void GetAllCountries_AddFewCountries()
+        {
+            //Arrange 
+            List<CountryAddRequest> country_request_list = new List<CountryAddRequest>()
+            {
+                new CountryAddRequest() { CountryName = "USA"},
+                new CountryAddRequest() { CountryName = "UK"}
+            };
+
+            //Act
+            List<CountryResponse> countries_list_from_add_country = new List<CountryResponse>();
+            foreach (CountryAddRequest countryAddRequest in country_request_list)
+            {
+                countries_list_from_add_country.Add(_countriesService.AddCountry(countryAddRequest));
+            }
+
+            List<CountryResponse> actualCountryResponseList = _countriesService.GetAllCountries();
+
+            //read each element from countries_list_from_add_country
+            foreach (CountryResponse expected_country in countries_list_from_add_country)
+            {
+                Assert.Contains(expected_country, actualCountryResponseList);
+            }
+        }
+
+        #endregion
+
+        #region GetCountryByCountryID
+
+        [Fact]
+        // If we supply null as countryID, it should return null as CountryResponse 
+        public void GetCountryByCountryID_NullCountryID()
+        {
+            //Arrange 
+            Guid? countryID = null;
+
+            //Act 
+            CountryResponse countryResponseFromGetMethod =
+                _countriesService.GetCountryByCountryID(countryID);
+    
+            //Assert
+            Assert.Null(countryResponseFromGetMethod);
+        }
+
+
+        [Fact]
+        // If we supply a valid country id, it should return the matching country details as CountryResponse object
+        public void GetCountryID_ValidCountryID()
+        {
+            //Arrange 
+            CountryAddRequest? countryAddRequest = new CountryAddRequest()
+            {
+                CountryName = "China"
+            };
+            CountryResponse countryResponseFromAddrequest = _countriesService.AddCountry(countryAddRequest);
+            
+            //Act
+            CountryResponse countryResponseFromGet = _countriesService.GetCountryByCountryID(countryResponseFromAddrequest.CountryID);
+
+            //Assert 
+            Assert.Equal(countryResponseFromAddrequest,countryResponseFromGet);
+        }
+
+        
+        #endregion
+    }
+}
+
---
+++ src/CRUD_Operations/Services/PersonService.cs
@@ -0,0 +1,236 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services.Helpers;
+
+namespace Services
+{
+    public class PersonService : IPersonService
+    {
+        //private fields
+        private readonly List<Person> _people;
+        private readonly ICountriesService _countriesService;
+        //constructor 
+        public PersonService()
+        {
+            _people = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            PersonResponse personResponse = person.ToPersonResponse();
+            personResponse.Country = _countriesService.GetCountryByCountryID(person.CountryID)?.CountryName;
+            return personResponse;
+        }
+
+        public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
+        {
+            //check if PersonAddRequest is not null
+            if (personAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+
+            //Model validations
+            ValidationHelper.ModelValidation(personAddRequest);
+
+            // convert personAddRequest into Person type 
+            Person person = personAddRequest.ToPerson();
+
+            //generate personID
+            person.PersonID = Guid.NewGuid();
+
+            //add person object to people list
+            _people.Add(person);
+
+            // Convert the Person object into PersonResponse type
+            return ConvertPersonToPersonResponse(person);
+        }
+
+        public List<PersonResponse> GetAllPeople()
+        {
+            return _people.Select(temp => temp.ToPersonResponse()).ToList();
+        }
+
+        public PersonResponse? GetPersonByPersonID(Guid? personID)
+        {
+            if (personID == null)
+            {
+                return null; 
+            }
+
+            Person? person = _people.FirstOrDefault(temp => temp.PersonID == personID);
+            if (person == null)
+            {
+                return null;
+            }
+
+            return person.ToPersonResponse();
+        }
+
+        public List<PersonResponse> GetFilteredPeople(string searchBy, string? searchString)
+        {
+            List<PersonResponse> allPeople = GetAllPeople();
+            List<PersonResponse> matchingPeople = allPeople;
+
+            if (string.IsNullOrEmpty(searchBy) || string.IsNullOrEmpty(searchString))
+            {
+                return matchingPeople;
+            }
+
+            switch (searchBy)
+            {
+                case nameof(Person.PersonName):
+                    matchingPeople = allPeople.Where(temp => 
+                        (string.IsNullOrEmpty(temp.PersonName) || temp.PersonName.Contains(searchString, StringComparison.OrdinalIgnoreCase))).ToList(); ;
+                    break;
+
+                case nameof(Person.Email):
+                    matchingPeople = allPeople.Where(temp =>
+                        (string.IsNullOrEmpty(temp.Email) || temp.Email.Contains(searchString, StringComparison.OrdinalIgnoreCase))).ToList(); ;
+                    break;
+
+                case nameof(Person.DateOfBirth):
+                    matchingPeople = allPeople.Where(temp =>
+                        (temp.DateOfBirth == null || temp.DateOfBirth.Value.ToString("dd MMMM yyy").Contains(searchString, StringComparison.OrdinalIgnoreCase))).ToList(); ;
+                    break;
+
+                case nameof(Person.Gender):
+                    matchingPeople = allPeople.Where(temp =>
+                        (string.IsNullOrEmpty(temp.Gender) || temp.Gender.Contains(searchString, StringComparison.OrdinalIgnoreCase))).ToList(); ;
+                    break;
+
+                case nameof(Person.CountryID):
+                    matchingPeople = allPeople.Where(temp =>
+                        (string.IsNullOrEmpty(temp.Country) || temp.Country.Contains(searchString, StringComparison.OrdinalIgnoreCase))).ToList(); ;
+                    break;
+
+                case nameof(Person.Address):
+                    matchingPeople = allPeople.Where(temp =>
+                        (string.IsNullOrEmpty(temp.Address) || temp.Address.Contains(searchString, StringComparison.OrdinalIgnoreCase))).ToList(); ;
+                    break;
+                default:
+                    matchingPeople = allPeople;
+                    break;
+            }
+
+            return matchingPeople;
+        }
+
+        public List<PersonResponse> GetSortedPeople(List<PersonResponse> allPeople, string sortBy, SortOrderOptions sortOrder)
+        {
+            if (string.IsNullOrEmpty(sortBy))
+            {
+                return allPeople;
+            }
+
+            List<PersonResponse> sortedPeople = (sortBy, sortOrder)
+                switch
+                {
+                    (nameof(PersonResponse.PersonName), SortOrderOptions.ASC) 
+                        => allPeople.OrderBy(temp => temp.PersonName, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.PersonName), SortOrderOptions.DESC) 
+                        => allPeople.OrderByDescending(temp => temp.PersonName, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.Email), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.Email).ToList(),
+
+                    (nameof(PersonResponse.Email), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.Email).ToList(),
+
+                    (nameof(PersonResponse.DateOfBirth), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.DateOfBirth).ToList(),
+
+                    (nameof(PersonResponse.DateOfBirth), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.DateOfBirth).ToList(),
+
+                    (nameof(PersonResponse.Age), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.Age).ToList(),
+
+                    (nameof(PersonResponse.Age), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.Age).ToList(),
+
+                    (nameof(PersonResponse.Gender), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.Gender, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.Gender), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.Gender, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.Country), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.Country, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.Country), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.Country, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.Address), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.Address, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.Address), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.Address, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                    (nameof(PersonResponse.ReceiveNewsLetters), SortOrderOptions.ASC)
+                        => allPeople.OrderBy(temp => temp.ReceiveNewsLetters).ToList(),
+
+                    (nameof(PersonResponse.ReceiveNewsLetters), SortOrderOptions.DESC)
+                        => allPeople.OrderByDescending(temp => temp.ReceiveNewsLetters).ToList(),
+
+                    _ => allPeople
+                };
+
+            return sortedPeople;
+        }
+
+        public PersonResponse UpdatePerson(PersonUpdateRequest? personUpdateRequest)
+        {
+            if (personUpdateRequest == null)
+            {
+                throw new ArgumentNullException(nameof(Person));
+            }
+
+            //validation 
+            ValidationHelper.ModelValidation(personUpdateRequest);
+
+            //get matching person object to update
+            Person? matchingPerson = _people.FirstOrDefault(temp => temp.PersonID == personUpdateRequest.PersonID);
+
+            if (matchingPerson == null)
+            {
+                throw new ArgumentException("Given person doesn't exist");
+            }
+
+            //Update all details
+            matchingPerson.PersonName = personUpdateRequest.PersonName;
+            matchingPerson.Email = personUpdateRequest.Email;
+            matchingPerson.DateOfBirth = personUpdateRequest.DateOfBirth;
+            matchingPerson.Gender = personUpdateRequest.Gender.ToString();
+            matchingPerson.CountryID = personUpdateRequest.CountryID;
+            matchingPerson.Address = personUpdateRequest.Address;
+            matchingPerson.ReceiveNewsLetters = personUpdateRequest.ReceiveNewsLetters;
+
+            return matchingPerson.ToPersonResponse();
+        }
+
+        public bool DeletePerson(Guid? personID)
+        {
+            if (personID == null)
+            {
+                throw new ArgumentNullException(nameof(personID));
+            }
+
+            Person? person = _people.FirstOrDefault(temp => temp.PersonID == personID);
+            if (person == null)
+            {
+                return false; 
+            }
+
+            _people.RemoveAll(temp => temp.PersonID == personID);
+
+            return true;
+        }
+    }
+}
