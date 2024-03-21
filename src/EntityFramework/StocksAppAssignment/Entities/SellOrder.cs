c11af4125710a4377937a0af5a54b0e70508137b
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-03-21 15:40:17 -0600

    added seed data for SellOrder and BuyOrder Entities

---
+++ src/EntityFramework/StocksAppAssignment/Entities/SellOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Entities
+{
+    /// <summary>
+    /// Domain model for selling an order
+    /// </summary>
+    public class SellOrder
+    {
+        [Key]
+        public Guid? SellOrderID;
+
+        [StringLength(10)]
+        public string? StockSymbol { get; set; }
+
+        [StringLength(40)]
+        public string? StockName { get; set; }
+        public DateTime? DateAndTimeOfOrder { get; set; }
+        public uint? Quantity { get; set; }
+        public double? Price { get; set; }
+    }
+}
---
+++ src/EntityFramework/StocksAppAssignment/Entities/StockMarketDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+namespace Entities
+{
+    public class StockMarketDbContext : DbContext
+    {
+        public StockMarketDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BuyOrder> BuyOrders { get; set; }
+        public DbSet<SellOrder> SellOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BuyOrder>().ToTable("BuyOrders");
+            modelBuilder.Entity<SellOrder>().ToTable("SellOrders");
+
+            //Seed to BuyOrders 
+            string buyOrdersJson = File.ReadAllText("BuyOrders.json");
+            List<BuyOrder>? buyOrders = JsonSerializer.Deserialize<List<BuyOrder>>(buyOrdersJson);
+
+            if (buyOrders != null)
+            {
+                foreach (BuyOrder buyOrder in buyOrders)
+                {
+                    modelBuilder.Entity<BuyOrder>().HasData(buyOrder);
+                }
+            }
+
+            //Seed to SellOrders 
+            string sellOrdersJson = File.ReadAllText("SellOrders.json");
+            List<SellOrder>? sellOrders = JsonSerializer.Deserialize<List<SellOrder>>(sellOrdersJson);
+
+            if (sellOrders != null)
+            {
+                foreach (SellOrder sellOrder in sellOrders)
+                {
+                    modelBuilder.Entity<BuyOrder>().HasData(sellOrder);
+                }
+            }
+        }
+    }
+}
