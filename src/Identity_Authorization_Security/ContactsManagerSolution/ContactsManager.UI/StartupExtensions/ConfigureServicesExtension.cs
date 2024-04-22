92c6bc05ab45e3b26657b7ab0770273f65520c54
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-21 20:36:50 -0600

    Added Password Complexity Configuration in ConfigureServicesExtension

--- src/Identity_Authorization_Security/ContactsManagerSolution/ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
+++ src/Identity_Authorization_Security/ContactsManagerSolution/ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
@@ -55,7 +55,15 @@ namespace ContactsManager.UI.StartupExtensions
             services.AddTransient<PeopleListActionFilter>();
 
             // Enable Identity in this project
-            services.AddIdentity<ApplicationUser, ApplicationRole>()
+            services.AddIdentity<ApplicationUser, ApplicationRole>((options) =>
+                {
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredUniqueChars = 3; //Eg: AB12AB
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders()
                 .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
