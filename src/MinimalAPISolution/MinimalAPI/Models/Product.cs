b1f4a8452fcce994339f83ec2c78c23bd6cbf983
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-15 01:17:27 -0600

    Added mapgroups

--- src/MinimalAPISolution/MinimalAPI/Models/Product.cs
+++ src/MinimalAPISolution/MinimalAPI/Models/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public int Id { get; set; }
     public string ProductName { get; set; }
 
-    public override string ToString()
+    public override string? ToString()
     {
         return $"Product ID: {Id}, Product Name: {ProductName}";
     }
--- src/MinimalAPISolution/MinimalAPI/Program.cs
+++ src/MinimalAPISolution/MinimalAPI/Program.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using MinimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,17 +11,31 @@ List<Product> list = new List<Product>()
     new Product() { Id = 1, ProductName = "Smart Phone"},
     new Product() { Id = 2, ProductName = "Smart TV "}
 };
+
+var mapGroup = app.MapGroup("/products");
 // GET /products 
-app.MapGet("/products", async (HttpContext context) =>
+mapGroup.MapGet("/products", async (HttpContext context) =>
 {
-    var content = string.Join("\n", list.Select(temp => temp.ToString()));
     // 1, xxxxxxxx
     // 2, xxxxxxxx
 
-    await context.Response.WriteAsync(content);
+    await context.Response.WriteAsync(JsonSerializer.Serialize(list));
 });
 
-app.MapPost("/", async (HttpContext context, Product product) =>
+// GET /products/{id}
+mapGroup.MapGet("/products/{id:int}", async (HttpContext context, int id) =>
+{
+    Product? product = list.FirstOrDefault(temp => temp.Id == id);
+    if (product == null)
+    {
+        context.Response.StatusCode = 400; //Bad request 
+        await context.Response.WriteAsync("Incorrect product ID");
+        return;
+    }
+    await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+}); 
+
+mapGroup.MapPost("/products", async (HttpContext context, Product product) =>
 {
     list.Add(product); 
     await context.Response.WriteAsync("Product added");
